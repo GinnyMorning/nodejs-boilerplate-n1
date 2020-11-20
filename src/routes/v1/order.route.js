3fbee4955e797274f24aecaaf1713583de0d0cd9
Toan PLQ <quoctoan.tn05@gmail.com>
2020-11-20 11:06:17 +0700

    fix bug order

--- src/routes/v1/order.route.js
+++ src/routes/v1/order.route.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const { orderValidation } = require('../../validations/index');
 const { orderController } = require('../../controllers/index');
 
 const router = express.Router();
 
-router.post('/create-order', validate(orderValidation.createOrder), orderController.createOrder);
-router.get('/get-order/:orderId', validate(orderValidation.getOrderById), orderController.getOrderById);
-router.put('/update-order/:orderId', validate(orderValidation.updateOrderById), orderController.updateOrderById);
-router.delete('/delete-order/:orderId', validate(orderValidation.deleteOrder), orderController.deleteOrder);
+router.route('/').post(auth('getUsers'), validate(orderValidation.createOrder), orderController.createOrder);
+
+router
+  .route('/:orderId')
+  .get(auth('getUsers'), validate(orderValidation.getOrderById), orderController.getOrderById)
+  .patch(auth('getUsers'), validate(orderValidation.updateOrderById), orderController.updateOrderById)
+  .delete(auth('getUsers'), validate(orderValidation.deleteOrder), orderController.deleteOrder);
 
 module.exports = router;

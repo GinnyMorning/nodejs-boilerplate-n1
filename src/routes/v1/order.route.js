bf8dbe27c78b8b00a754064e7fbb5adc87407174
Toan PLQ <quoctoan.tn05@gmail.com>
2020-11-20 14:41:29 +0700

    fix bug order

--- src/routes/v1/order.route.js
+++ src/routes/v1/order.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const auth = require('../../middlewares/auth');
-const validate = require('../../middlewares/validate');
-const { orderValidation } = require('../../validations/index');
+// const validate = require('../../middlewares/validate');
+// const { orderValidation } = require('../../validations/index');
 const { orderController } = require('../../controllers/index');
 
 const router = express.Router();
 
-router.route('/').post(auth('getUsers'), validate(orderValidation.createOrder), orderController.createOrder);
+router.route('/').post(auth('getUsers'), null, orderController.createOrder);
 
 // router
 //   .route('/:orderId')

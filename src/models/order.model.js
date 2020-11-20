3ed5482ef8aa0bacd7aca80f5517699b279da521
Toan PLQ <quoctoan.tn05@gmail.com>
2020-11-20 17:04:20 +0700

    fix bug order

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -2,28 +2,33 @@ const mongoose = require('mongoose');
 const orderStatus = require('../config/orderStatus');
 const { toJSON } = require('./plugins');
 
-const orderSchema = mongoose.Schema({
-  userId: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'User',
-    // required: true,
+const orderSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      // required: true,
+    },
+    // orderId: {
+    //   type: mongoose.SchemaTypes.ObjectId,
+    //   require: true,
+    //   trim: true,
+    //   index: true,
+    // },
+    productIds: {
+      type: String,
+      require: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: [orderStatus.NEW, orderStatus.ACCEPTED, orderStatus.PROCCESSING, orderStatus.COMPLEDTED, orderStatus.CANCEL],
+      require: true,
+    },
   },
-  // orderId: {
-  //   type: mongoose.SchemaTypes.ObjectId,
-  //   require: true,
-  //   trim: true,
-  //   index: true,
-  // },
-  productIds: {
-    type: String,
-    require: true,
-  },
-  orderStatus: {
-    type: String,
-    enum: [orderStatus.NEW, orderStatus.ACCEPTED, orderStatus.PROCCESSING, orderStatus.COMPLEDTED, orderStatus.CANCEL],
-    require: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // add plugin that converts mongoose to json
 orderSchema.plugin(toJSON);
